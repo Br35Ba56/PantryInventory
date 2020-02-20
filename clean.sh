a5f4b5822fe18208bca22c6a31708ef9ccb65669
vagrant <vagrant@vagrant.vm>
2020-02-20 23:34:17 +0000

    fixed dir not found clean.sh

--- clean.sh
+++ clean.sh
@@ -3,4 +3,4 @@
 rm /vagrant/PantryInventory/PanInv-sqlite
 python /vagrant/PantryInventory/manage.py makemigrations
 python /vagrant/PantryInventory/manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'admin@myproject.com', 'root')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'admin@myproject.com', 'root')" | python /vagrant/PantryInventory/manage.py shell
