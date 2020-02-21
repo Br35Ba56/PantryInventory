680d8c00cf7747cb16a3afb2e193730251f1f4c0
vagrant <vagrant@vagrant.vm>
2020-02-21 00:31:45 +0000

    fixed bug clean.sh

--- clean.sh
+++ clean.sh
@@ -4,6 +4,5 @@ if [[ -f /vagrant/PantryInventory/PanInv-sqlite ]]
 then
     rm -f /vagrant/PantryInventory/PanInv-sqlite
 fi
-python /vagrant/PantryInventory/manage.py makemigrations
-python /vagrant/PantryInventory/manage.py migrate
+cd /vagrant/PantryInventory/ && python manage.py makemigrations && python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'admin@myproject.com', 'root')" | python /vagrant/PantryInventory/manage.py shell
