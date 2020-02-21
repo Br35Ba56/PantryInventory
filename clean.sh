f202c3fa20b06930c1611268a437d293a7626ba2
vagrant <vagrant@vagrant.vm>
2020-02-21 00:24:06 +0000

    check for file before delete

--- clean.sh
+++ clean.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-rm /vagrant/PantryInventory/PanInv-sqlite
+if [[ -f /vagrant/PantryInventory/PanInv-sqlite ]]
+then
+    rm -f /vagrant/PantryInventory/PanInv-sqlite
+fi
 python /vagrant/PantryInventory/manage.py makemigrations
 python /vagrant/PantryInventory/manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'admin@myproject.com', 'root')" | python /vagrant/PantryInventory/manage.py shell
