154b46559a23252c6a889252f617083569058ce0
vagrant <vagrant@vagrant.vm>
2020-02-22 02:08:13 +0000

    acq/exp date required

--- api/models.py
+++ api/models.py
@@ -9,8 +9,8 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity_with_unit = models.CharField(max_length=100, null=True)
-    acquisition_date = models.DateField(auto_now=False, null=True)
-    expiration_date = models.DateField(auto_now=False, null=True)
+    acquisition_date = models.DateField(auto_now=True)
+    expiration_date = models.DateField(auto_now=False)
 
     def __str__(self):
         return self.name + ' ' + str(self.acquisition_date)
