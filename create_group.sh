f860d99e3226df12e96d12358eb8f566921f2a4d
vagrant <vagrant@vagrant.vm>
2020-02-25 00:40:42 +0000

    fixed bug in create_group.sh

--- create_group.sh
+++ create_group.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/vagrant/python/env/bin/activate
-python /vagrant/PantryInventory/manage.py create_group
+cd /vagrant/PantryInventory && python /vagrant/PantryInventory/manage.py create_group
