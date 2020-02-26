63b053910c08c6baeb2dd2eef8dcfab60f430496
jsmeb <59983852+jsmeb@users.noreply.github.com>
2020-02-26 18:57:22 -0500

    added users to group
    on generating user test data, they are now also added the api_user_group

--- api/management/commands/create_items.py
+++ api/management/commands/create_items.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from api.models import Item
@@ -9,8 +10,9 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         fake = Faker()
+        my_group = Group.objects.get(name='api_user_group')
         for _ in range(0,10):
             new_user = User.objects.create(username=fake.first_name()+fake.last_name(), password=fake.word(ext_word_list=None)+fake.word(ext_word_list=None))
-            new_user.save()
+            my_group.user_set.add(new_user)
             for _ in range(0,10):
               Item.objects.create(name=fake.word(ext_word_list=None), user_id = new_user, quantity_with_unit=fake.word(ext_word_list=None), acquisition_date=fake.date(pattern='%Y-%m-%d', end_datetime=None), expiration_date=fake.date(pattern='%Y-%m-%d', end_datetime=None))  
