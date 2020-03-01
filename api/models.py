a6b30c601ea70ac20c7cc2f08128769c00422e08
jsmeb <59983852+jsmeb@users.noreply.github.com>
2020-03-01 18:51:12 -0500

    Updated models.py
    Updated models.py acquisition_date parameters

--- api/models.py
+++ api/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
-
-
-
+from django.utils.timezone import now
+from datetime import date
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity_with_unit = models.CharField(max_length=100, null=True, blank=True)
-    acquisition_date = models.DateField(auto_now=True)
+    acquisition_date = models.DateField(null=False, blank=False)
     expiration_date = models.DateField(auto_now=False)
 
     def __str__(self):
